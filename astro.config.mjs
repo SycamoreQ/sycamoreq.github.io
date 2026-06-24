494796a0bf1245a2e4443cfa14e6ff8901f0a240
SycamoreQ <kaushik.80405@gmail.com>
2026-06-24 20:27:32 +0530

    feat(blog): fixed blog issues and deployed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://sycamoreq.github.io',
   trailingSlash: 'never',
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [
+      [rehypeKatex, { strict: false }],
+    ],
+  },
 });
+
